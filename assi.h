096e31ea839b04ced10246762a3d23571114f292
myriaiz <138396898+myriaiz@users.noreply.github.com>
2023-12-06 19:31:42 -0300

    assi.h

---
+++ assi.h
@@ -0,0 +1,17 @@
+#include <algorithm> //para sort
+#include <chrono>
+#include <ctime>
+#include <ctype.h> //para o isdigit
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <string> //substr e stoi
+#include <vector>
+
+using namespace std;
+using namespace std::chrono;
+
+const string linha;
+
+
+int tira(const string linha);
